0cecb34a837a21ab8ee3d7077f8fb015d7e4c20f
Ayu Saniatus Sholihah <AyuSaniatusSholihah@users.noreply.github.com>
2026-05-14 11:17:00 +0700

    feat: update user migration and model for OTP and phone number

--- src/database/migrations/0001_01_01_000000_create_users_table.php
+++ src/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,8 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('google_id')->nullable();
+            $table->text('google_token')->nullable();
+            $table->text('google_refresh_token')->nullable();
             $table->string('name');
+            $table->string('phone_number')->nullable();
             $table->string('email')->unique();
+            $table->string('otp_code')->nullable();
+            $table->timestamp('otp_expires_at')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
