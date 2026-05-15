1dcb3f1cacde7bbcca8f881ecce3bc83cef4f2d6
Ayu Saniatus Sholihah <AyuSaniatusSholihah@users.noreply.github.com>
2026-05-15 08:53:27 +0700

    delete folder sewain

--- src/resources/views/auth/login.blade.php
+++ src/resources/views/auth/login.blade.php
@@ -152,13 +152,13 @@
                         <path d="M5.84 14.09c-.22-.66-.35-1.36-.35-2.09s.13-1.43.35-2.09V7.07H2.18C1.43 8.55 1 10.22 1 12s.43 3.45 1.18 4.93l3.66-2.84z" fill="#FBBC05"/>
                         <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
                     </svg>
-                    Sign up with Google
+                    Sign in with Google
                 </a>
                 <button class="social-btn">
                     <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
                     </svg>
-                    Sign up with Email
+                    Sign in with Email
                 </button>
             </div>
 
